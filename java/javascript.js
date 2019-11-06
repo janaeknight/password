7e706fb508d3601ad3787086a24d2c77d5a65ab0
Knight <janae.jpeg@gmail.com>
2019-11-06 16:36:10 -0600

    gen update

--- java/javascript.js
+++ java/javascript.js
@@ -7,10 +7,12 @@ function generalFunction() {
 
 
 
-    if (password.length>=9) {
-        console.log("The password "+password, " has " +password.length, "characters and meets minimum requirements." );
+    if (password.length>=9 &&password.length<=20) {
+        console.log("The password "+password, " has " +password.length, "characters and meets minimum requirements.");
+    } else if (password.length>20) {
+        console.log("The password "+password, " has " +password.length, "characters. Maximum is 20 characters." );
     } else {
-        console.log("The password "+password, "only has " +password.length, "characters. Minimum is 8 characters." );
+        console.log("The password "+password, "only has " +password.length, "characters. Minimum is 8 characters.");
     };
 
 
@@ -20,6 +22,9 @@ function generalFunction() {
         console.log("The password" +password, "is missing a special character.");
     };
  
+    if (password.includes(" ")) {
+        console.log("Password contains an illegal Character.");
+    }
 
     return;
 

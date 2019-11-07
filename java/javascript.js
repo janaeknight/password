bd663e62ed92da2820a2785777dab7496a3600c1
Knight <janae.jpeg@gmail.com>
2019-11-06 18:04:45 -0600

    gen update

--- java/javascript.js
+++ java/javascript.js
@@ -21,11 +21,42 @@ function generalFunction() {
     } else {
         console.log("The password" +password, "is missing a special character.");
     };
- 
-    if (password.includes(" ")) {
-        console.log("The password " +password, "contains an illegal Character.");
-    }
+
+    if ( (password.includes("A")) || (password.includes("B")) || (password.includes("C")) || (password.includes("D")) || (password.includes("E")) || (password.includes("F")) || (password.includes("G")) || (password.includes("H")) || (password.includes("I")) || (password.includes("J")) || (password.includes("K")) || (password.includes("L")) || (password.includes("M")) || (password.includes("N")) || (password.includes("O")) || (password.includes("P")) || (password.includes("Q")) || (password.includes("R")) || (password.includes("S")) || (password.includes("T")) || (password.includes("U")) || (password.includes("V")) || (password.includes("W")) || (password.includes("X")) || (password.includes("Y")) || (password.includes("Z")) ) {
+        console.log("The password " +password, "contains a capital letter.");
+    } else {
+        console.log("The password " +password, "is missing a capital letter.");
+    };
+
+    if ( (password.includes("a")) || (password.includes("b")) || (password.includes("c")) || (password.includes("d")) || (password.includes("e")) || (password.includes("f")) || (password.includes("g")) || (password.includes("h")) || (password.includes("i")) || (password.includes("j")) || (password.includes("k")) || (password.includes("l")) || (password.includes("m")) || (password.includes("n")) || (password.includes("o")) || (password.includes("p")) || (password.includes("q")) || (password.includes("r")) || (password.includes("s")) || (password.includes("t")) || (password.includes("u")) || (password.includes("v")) || (password.includes("w")) || (password.includes("x")) || (password.includes("y")) || (password.includes("z"))) {
+        console.log("The password " +password, "contains a lowercase letter.");
+    } else {
+        console.log("The password " +password, "is missing a lowercase letter.");
+    };
+
+    function checkNumbers() {
+        if ( (password.includes("1")) || password.includes("2") || password.includes("3") || password.includes("4") || password.includes("5") || password.includes("6") || password.includes("7") || password.includes("8") || password.includes("9") || password.includes("0") ) {
+            console.log("The password " +password, "contains at least one number.");
+            return true;
+        } else {
+            console.log("The password " +password, "is missing a number.");
+            return false;
+        };
+    };
+
+    function checkSpaces() {
+        if (password.includes(" ")) {
+            console.log("The password " +password, "contains an illegal Character.");
+            return true;
+        }
+    };
+
+
+////////////////////////////////////////////////////////////////////////////
+    
+
+
+
 
     return;
-
 };

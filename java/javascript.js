30ce12a87ad64133f0b281ffbacd1c4759fb95de
Knight <janae.jpeg@gmail.com>
2019-11-08 07:42:06 -0600

    gen update

--- java/javascript.js
+++ java/javascript.js
@@ -26,7 +26,7 @@ function generalFunction() {
             return true;
         } else {
             console.log("The password "+password, "has " +password.length, "characters. Min - 8. Max - 20" );
-            //noticeMinMax.style.display = "block";
+            noticeMinMax.style.display = "block";
             return false;
         };
     };
@@ -37,10 +37,13 @@ function generalFunction() {
             return true;
         } else {
             console.log("The password " +password, "is missing a special character.");
-            //noticeSpecial.innerhtml("here")
+            noticeSpecial.style.display = "block";
             return false;
         };
     };
+
+    checkLength();
+    checkSpecial();
 
     function checkUppercase() {
         if ( (password.includes("A")) || (password.includes("B")) || (password.includes("C")) || (password.includes("D")) || (password.includes("E")) || (password.includes("F")) || (password.includes("G")) || (password.includes("H")) || (password.includes("I")) || (password.includes("J")) || (password.includes("K")) || (password.includes("L")) || (password.includes("M")) || (password.includes("N")) || (password.includes("O")) || (password.includes("P")) || (password.includes("Q")) || (password.includes("R")) || (password.includes("S")) || (password.includes("T")) || (password.includes("U")) || (password.includes("V")) || (password.includes("W")) || (password.includes("X")) || (password.includes("Y")) || (password.includes("Z")) ) {
